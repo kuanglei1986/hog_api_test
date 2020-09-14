1d8ae57b38cc40721898642785a38933b1bf3b0a
kuanglei1986 <344960239@qq.com>
2020-09-14 09:29:08 +0800

    编写测试用例代码

--- ServiceTesting/src/test/java/com/kl/wework/contact/DepartmentTest.java
+++ ServiceTesting/src/test/java/com/kl/wework/contact/DepartmentTest.java
@@ -3,6 +3,7 @@ package com.kl.wework.contact;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepartmentTest {
@@ -13,5 +14,9 @@ class DepartmentTest {
 
     @Test
     void list() {
+        Department department = new Department();
+        department.list("2").then().statusCode(200)
+        .body("department.name[0]", equalTo("广州研发中心"));
+
     }
 }
